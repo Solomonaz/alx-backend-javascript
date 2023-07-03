111fb7899cb1e6f217008c46b32e74a44498cf68
Solomon <solomonaz2022@gmail.com>
2023-07-03 11:00:24 +0000

    JAvaSCRIPT manipulation

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -5,9 +5,11 @@ export function uploadPhoto() {
   });
 }
 
+
 export function createUser() {
   return Promise.resolve({
     firstName: 'Guillaume',
     lastName: 'Salva',
   });
 }
+
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Solomon Azene <https://github.com/Solomonaz>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
